923b8a2d6bfd61a09fed52e9a0290c802988a969
nshout <nshout99@gmail.com>
2024-03-26 20:26:23 +0100

    Arc Changes. Styling and background drop. Untested.

--- {{cookiecutter.out_dir}}/lib/main.dart
+++ {{cookiecutter.out_dir}}/lib/main.dart
@@ -90,7 +90,7 @@ Future<void> _configureMacosWindowUtils() async {
   await config.apply();
 }
 
-void main() async {
+Future<void> main() async {
   if (isProduction) {
     // ignore: avoid_returning_null_for_void
     debugPrint = (String? message, {int? wrapWidth}) => null;
@@ -161,7 +161,13 @@ void main() async {
 //     });
 //   }
 
+  WidgetsFlutterBinding.ensureInitialized();
+  await Window.initialize();
+
   if (Platform.isMacOS) {
+    await Window.setEffect(
+      effect: WindowEffect.fullScreenUI,
+    );
     await _configureMacosWindowUtils();
   }
 }
